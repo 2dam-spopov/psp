4c56c207f4a259bcf78468bb37a0cbe782930bf1
2dam-spopov <43165147+2dam-spopov@users.noreply.github.com>
2019-01-16 23:32:05 +0100

    LanzaHilos213

---
+++ LanzaHilos213.java
@@ -0,0 +1,36 @@
+
+public class LanzaHilos {
+	public static void main(String[] args) {
+		System.out.println("Iniciando hilo hijo " + Thread.currentThread().getName());
+		new HiloRecursivo().start();
+	}
+}
+
+class HiloRecursivo extends Thread {
+	
+	public static int numero;
+	public static boolean flag = false;
+	private static int siguiente;
+	
+	public HiloRecursivo() {
+		if(!flag) {
+			this.numero = 10;
+			flag = true;
+		}else {
+			this.numero = siguiente;
+		}
+	}
+
+	public void run() {
+		if(numero > 0) {
+			this.setName("Hilo " + numero);
+			this.siguiente = numero - 1;
+			System.out.println("Soy el "+ getName() + " y voy a llamar al hilo " + (numero + 1));
+			HiloRecursivo hilo = new HiloRecursivo();
+			hilo.start();
+		}else if(numero == 0) {
+			this.setName("Hilo " + numero);
+			System.out.println("Soy el " + getName());
+		}
+	}
+}
